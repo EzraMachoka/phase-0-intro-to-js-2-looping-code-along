c72b5998ad015ca511dd162c8b9a859cb13fcf36
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-06-22 14:00:29 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+
+    const messages = [];
+    for (let n = 0; n < names.length; n++) {
+      const gratitude = `Thank you, ${names[n]}, for the wonderful ${event} gift!`;
+      messages.push(gratitude);
+    }
+
+    return messages;
+  }
+
+function countDown(Number){
+    for (let n = Number; n>=0; n--) {
+    console.log(n);
+    
+}
+
+}
